3c8c0c0731d24eb42d45d3e0ddf1c776f8eeef81
dskrenta <dskrenta@gmail.com>
2018-02-22 00:55:26 -0700

    frontend updates

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { createStore, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import { Provider } from 'react-redux';
+
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({
+  form: formReducer
+});
+
+const store = createStore(rootReducer);
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:3000/graphql' }),
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <MuiThemeProvider>
+        <App />
+      </MuiThemeProvider>
+    </Provider>
+  </ApolloProvider>, 
+  document.getElementById('root')
+);
+
 registerServiceWorker();
