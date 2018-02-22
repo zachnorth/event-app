03e0a5e65cb713ef3493c8f1ab5b387ef0fd70ee
dskrenta <dskrenta@gmail.com>
2018-02-21 19:49:57 -0700

    lowercased index names to comply with es standards

--- server/src/utils/constants.js
+++ server/src/utils/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
-  EVENT_INDEX: 'eventAppEventIndex',
-  USER_INDEX: 'eventAppUserIndex',
+  EVENT_INDEX: 'event-app-event-index',
+  USER_INDEX: 'event-app-user-index',
   GOOGLE_CLIENT_ID: 'something',
   JWT_SECRET: 'xxx'
 };
