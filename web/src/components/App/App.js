5a0ae194b3e4d0c7ec6be06dae2a4acaab6dd601
dskrenta <dskrenta@gmail.com>
2018-02-22 06:25:45 -0700

    Basic example components

--- web/src/components/App/App.js
+++ web/src/components/App/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import AuthRoute from '../Common/AuthRoute';
+import Landing from '../Landing/Landing';
+import NotFound from '../Common/NotFound';
+import Events from '../Events/Events';
+import Event from '../Event/Event';
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path='/' component={Landing} />
+      <AuthRoute exact path='/search/:query' component={Events} />
+      <AuthRoute exact path='/event/:id' component={Event} />
+      <Route path ='/' component={NotFound} />
+    </Switch>
+  </Router>
+);
 
 export default App;
