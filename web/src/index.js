8a8a9710c36a4498b50a2a0a15f403c9da6eb6cb
dskrenta <dskrenta@gmail.com>
2018-02-22 18:06:09 -0700

    Added auth middleware for passing user token to server

--- web/src/index.js
+++ web/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink, concat } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -11,6 +12,7 @@ import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './components/App/App';
+import { getToken } from './utils/userState';
 import registerServiceWorker from './registerServiceWorker';
 
 const rootReducer = combineReducers({
@@ -19,8 +21,20 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
+const httpLink = new HttpLink({ uri: 'http://localhost:3000/graphql' });
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      authorization: getToken() || null
+    }
+  });
+
+  return forward(operation);
+});
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:3000/graphql' }),
+  link: concat(authMiddleware, httpLink),
   cache: new InMemoryCache()
 });
 
